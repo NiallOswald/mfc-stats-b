a49c5c01237065d4f3ebdc208bac0c348379c649
Niall Oswald <79726292+NiallOswald@users.noreply.github.com>
2024-12-24 13:32:12 +0000

    Feat: Update ballistic model script

--- assignment/scripts/ballistic_model.py
+++ assignment/scripts/ballistic_model.py
@@ -2,9 +2,11 @@
 
 import numpy as np
 from assignment.models import Model
-from assignment.filters import KalmanFilter
+from assignment.filters import KalmanFilter, ParticleFilter
 
 SEED = 1234
+MAX_ITER = 1000
+N_PARTICLES = 1000
 
 
 def main():
@@ -36,7 +38,17 @@ def main():
     )
 
     model = Model(np.zeros(4), A, Q, H, R, SEED)
-    KalmanFilter.plot(model, 3 * np.ones(4), 10 * np.eye(4), 1000)
+    KalmanFilter.plot(model, MAX_ITER, 3 * np.ones(4), 10 * np.eye(4), save_fig=True)
+    ParticleFilter.plot(
+        model,
+        MAX_ITER,
+        np.random.multivariate_normal(
+            np.array([0.0, 0.0, -10.0, -10.0]),
+            0.1 * np.eye(4),
+            N_PARTICLES,
+        ).T,
+        save_fig=True,
+    )
 
 
 if __name__ == "__main__":
